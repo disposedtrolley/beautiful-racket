48b7722f9302abb09c5cac2c82c9ad6de67e5f55
James Liu <james_liu@fastmail.com>
2019-08-11 17:53:54 +1000

    feat: functional implementation of stacker

--- funstacker/funstacker.rkt
+++ funstacker/funstacker.rkt
@@ -2,33 +2,33 @@
 
 (define (read-syntax path port)
   (define src-lines (port->lines port))
-  (define src-datums (format-datums '(handle ~a) src-lines))
+  (define src-datums (format-datums '~a src-lines))
   (define module-datum `(module funstacker-mod "funstacker.rkt"
-                          ,@src-datums))
+                          (handle-args ,@src-datums)))
   (datum->syntax #f module-datum))
 
 (provide read-syntax)
 
-(define-macro (funstacker-module-begin HANDLE-EXPR ...)
+(define-macro (funstacker-module-begin HANDLE-ARGS-EXPR)
   #'(#%module-begin
-     HANDLE-EXPR ...
-     (display (first stack))))
+     (display (first HANDLE-ARGS-EXPR))))
 (provide (rename-out [funstacker-module-begin #%module-begin]))
 
-(define stack empty)
-(define (pop-stack!)
-  (define arg (first stack))
-  (set! stack (rest stack))
-  arg)
-(define (push-stack! arg)
-  (set! stack (cons arg stack)))
-
-(define (handle [arg #f])
-  (cond
-   [(number? arg) (push-stack! arg)]
-   [(or (equal? + arg) (equal? * arg))
-    (define op-result (arg (pop-stack!) (pop-stack!)))
-    (push-stack! op-result)]))
-(provide handle)
+(define (handle-args . args)
+  ;; Iterate over inputs while maintaining an accumulator of the current result.
+  (for/fold ([stack-acc empty])
+      ([arg (in-list args)]
+       #:unless (void? arg))
+    ;; If the current value is a number, push it into the stack.
+    ;; If the current value is an operator, take the first and second values from the stack,
+    ;; perform the operation, pop the values off the stack, and then push the accumulator
+    ;; onto the stack.
+    (cond
+     [(number? arg) (cons arg stack-acc)]
+     [(or (equal? * arg) (equal? + arg))
+      (define op-result
+        (arg (first stack-acc) (second stack-acc)))
+      (cons op-result (drop stack-acc 2))])))
+(provide handle-args)
 
 (provide * +)
